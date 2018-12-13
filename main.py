be61ec5098899932c2854b713a2ad9998ec6164b
darunphop <darunphop.p@gmail.com>
2018-12-13 17:33:37 +0700

    added -200 value filter

--- main.py
+++ main.py
@@ -6,6 +6,13 @@ def preprocessData(file='AirQualityUCI.xlsx'):
     data = pd.read_excel(file).values
     print(data.shape)
     data = data[:,[3,6,8,10,11,12,13,14,5]]
+    print(data.shape)
+    res = []
+    for i in range(data.shape[0]):
+        if -200.0 in data[i]:
+            res.append(int(i))
+    data = np.delete(data, res, 0)
+    print(data.shape)
     pData = np.asarray([np.concatenate([data[l][:-1],[data[l+120][-1]],[data[l+240][-1]]]) for l in range(len(data)-240)])
     np.savetxt('processedData.txt', pData, fmt='%.13f')
 
@@ -14,3 +21,5 @@ if __name__ == '__main__':
 
     print(trainSet.shape)
     print(testSet.shape)
+
+    # preprocessData()
